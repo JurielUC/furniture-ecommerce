2c3e5d86748d8e0e6af6ae74881b56a9ad41968d
unknown <juriel.ucomia@gmail.com>
2022-08-16 14:58:30 +0800

    update

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gil's Furniture</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style/style.css">
+</head>
+<body>
+    <!--Header and divider-->
+    <header>
+        <div class="app-name">
+            <a href="">
+                <h1>Furniture eCommerce</h1>
+            </a>
+        </div>
+        <!--Put your navigation here below-->
+        <nav>
+            <a href="">Login</a>
+            <a href="">Signup</a>
+        </nav>
+    </header>
+    <div class="divider">
+        <p>Wood Furniture Design Customization and Ordering System</p>
+    </div>
+    <!------------------------------------------------>
+
+    <!--Main content-->
+    <main>
+        <section id="landing-page">
+            
+        </section>
+    </main>
+</body>
+</html>
