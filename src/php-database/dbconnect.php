dd98c0fdfe2e6a85e6e107ea78d881aa3d001598
unknown <juriel.ucomia@gmail.com>
2022-09-10 19:36:47 +0800

    update dbconnect.php

--- src/php-database/dbconnect.php
+++ src/php-database/dbconnect.php
@@ -7,7 +7,7 @@
     // Create connection
     $conn = mysqli_connect($servername, $username, $password);
 
-    mysqli_select_db($conn, "db_ecommerce");
+    mysqli_select_db($conn, $database);
 
     // Check connection
     if (!$conn) {
