ed6dfffd380bd7194c3e52142347f86b8c9864fc
unknown <juriel.ucomia@gmail.com>
2022-09-11 17:57:54 +0800

    update

--- src/sys-user/login-page/login-con.php
+++ src/sys-user/login-page/login-con.php
@@ -5,22 +5,21 @@
     if($_SERVER["REQUEST_METHOD"] == "POST") {
         $email=mysqli_real_escape_string($conn,$_POST['email']);
         $password=mysqli_real_escape_string($conn,$_POST['u_password']);
-        $result = mysqli_query($conn,"SELECT * FROM tb_user");
-        $c_rows = mysqli_num_rows($result);
-
-        if ($c_rows!=$email || $c_rows!=$password) {
-            header("location: userlogin.php?remark_login=failed");
-        }
 
         $sql="SELECT id FROM tb_user WHERE email='$email' and u_password='$password'";
         $result=mysqli_query($conn,$sql);
-        $row=mysqli_fetch_array($result);
-        $active=$row['active'];
+        $row=mysqli_fetch_array($result, MYSQLI_ASSOC);
+        $active=$row['active'] = '';
         $count=mysqli_num_rows($result);
 
         if($count==1) {
+            //session_register("email");
             $_SESSION['login_user']=$email;
             header("location: ../home-page/userhome.php");
+        } 
+        else { 
+            header("location: userlogin.php?remark_login=failed"); 
+            
         }
     }
 ?>
